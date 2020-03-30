95687b31f4944bcd25a61b2093cc580c4bcaee42
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-03-30 13:46:27 +0200

    Initial commit

---
+++ Barometer.cs
@@ -0,0 +1,61 @@
+﻿namespace WeatherStationV10
+{
+    /// <summary>
+    ///  Simple barometer, measuring pressure in hPa (hectopascal)
+    /// </summary>
+    public class Barometer
+    {
+        #region Instance fields
+        private double _pressureInHPa;
+        #endregion
+
+        #region Constructor
+        public Barometer()
+        {
+            _pressureInHPa = 1000.0;
+        }
+        #endregion
+
+        #region Properties
+        public double Pressure
+        {
+            get { return _pressureInHPa; }
+            set { _pressureInHPa = value; }
+        }
+
+        public string WeatherDescription
+        {
+            get
+            {
+                if(Pressure <980)
+                {
+                    return "Stormy";
+                }
+                else if(Pressure <1000)
+                {
+                    return "Rainy";
+                }
+                else if(Pressure <1020)
+                {
+                    return "Changing";
+                }
+                else if(Pressure <1040)
+                {
+                    return "Fair";
+                }
+                else
+                {
+                    return "Very Dry";
+                }
+            }
+        }
+        #endregion
+        // public static int Infinite(int Barometer)
+        // while (Barometer<2000)
+        // {
+        //     System.Console.WriteLine();
+        // }
+
+
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WeatherStationV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+            // System.Console.WriteLine($"The weather {Barometer.}");
+
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
